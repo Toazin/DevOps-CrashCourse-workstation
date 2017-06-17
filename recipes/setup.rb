0a70de57df3afa94dcf16186b5e3583e47a8912f
ChefDK User <chef@ip-10-0-0-186.ec2.internal>
2017-06-17 22:22:22 +0000

    Template added and provided in setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,15 +1,19 @@
 package 'tree' do
 	action :install
 end
-file '/etc/motd' do
-	action :create
-        content "Property of Toatzin Padilla
+#file '/etc/motd' do
+#	action :create
+#        content "Property of Toatzin Padilla
+#
+#	IPADDRESS: #{node['ipaddress']}
+#	HOSTNAME : #{node['hostname']}
+#	MEMORY   : #{node['memory']['total']}
+#	CPU      : #{node['cpu']['0']['mhz']}"
 
-	IPADDRESS: #{node['ipaddress']}
-	HOSTNAME : #{node['hostname']}
-	MEMORY   : #{node['memory']['total']}
-	CPU      : #{node['cpu']['0']['mhz']}"
+#end
 
+template '/etc/motd' do
+	source 'motd.erb'
 end
 
 
