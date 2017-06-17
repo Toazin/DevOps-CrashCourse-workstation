86d878ad3905f62e622f596bdbab9c4da73d226e
ChefDK User <chef@ip-10-0-0-186.ec2.internal>
2017-06-17 21:07:10 +0000

    Changes in setup.rb in /etc/motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,7 +3,13 @@ package 'tree' do
 end
 file '/etc/motd' do
 	action :create
-        content 'Property of Toatzin Padilla'
+        content "Property of Toatzin Padilla
+
+	IPADDRESS: #{node['ipaddress']}
+	HOSTNAME : #{node['hostname']}
+	MEMORY   : #{node['memory']['total']}
+	CPU      : #{node['cpu']['0']['mhz']}"
+
 end
 
 
